fc0e78a095f75c3f6e6cc58c93fcd4ac1496bd92
Kenny Nguyen <sokenns@gmail.com>
2024-01-05 00:20:28 -0800

    Create append function

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 class Node {
-  constructor(value, next) {
+  constructor(value = null, nextNode = null) {
     this.value = value;
-    this.next = next;
+    this.nextNode = nextNode;
   }
 }
 
 class LinkedList {
   constructor() {
-    this.nodes = {};
+    this.nodes = { head: null };
   }
 
-  append(value) {}
+  append(value) {
+    const newNode = new Node(value);
+    if (this.nodes.head === null) {
+      this.nodes.head = newNode;
+    } else {
+      let root = this.nodes.head;
+      while (root.nextNode) {
+        root = root.nextNode;
+      }
+      root.nextNode = newNode;
+    }
+  }
 
   prepend(value) {}
 
@@ -30,3 +41,11 @@ class LinkedList {
 
   toString() {}
 }
+
+let test = new LinkedList();
+test.append(1);
+test.append(2);
+test.append(3);
+console.log(test.nodes);
+
+console.log(test.nodes.head.nextNode.nextNode);
