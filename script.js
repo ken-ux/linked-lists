0ef5f263b60b1ac3ca9ff87a1a660bc5c08ad70c
Kenny Nguyen <sokenns@gmail.com>
2024-01-04 23:04:10 -0800

    Add node and linkedlist classes

---
+++ script.js
@@ -0,0 +1,33 @@
+class Node {
+  constructor(value, left, right) {
+    this.data = value;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.nodes = {};
+  }
+
+  append(value) {}
+
+  prepend(value) {}
+
+  size() {}
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+}
